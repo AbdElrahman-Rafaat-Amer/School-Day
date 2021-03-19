91cae100fc64c8c8e8cf846631b34e94feec7a9f
Ziad Nasser <55110484+ziad7411@users.noreply.github.com>
2021-03-20 00:35:40 +0200

    19/3

--- Andriod/SchoolDay/app/src/main/java/com/example/schoolday/FragmentFees.java
+++ Andriod/SchoolDay/app/src/main/java/com/example/schoolday/FragmentFees.java
@@ -4,19 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
-public class FragmentSubjects extends androidx.fragment.app.Fragment {
+public class FragmentFees extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view=inflater.inflate(R.layout.fragment_subjects,container,false);
-        final FrameLayout frameLayout = view.findViewById(R.id.fid);
-        CardView
+
+       View view = inflater.inflate(R.layout.fragment_fees,container,false);
+
+
+
         return view;
     }
 }
--- Andriod/SchoolDay/app/src/main/java/com/example/schoolday/FragmentOnline.java
+++ Andriod/SchoolDay/app/src/main/java/com/example/schoolday/FragmentOnline.java
@@ -4,19 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
-public class FragmentSubjects extends androidx.fragment.app.Fragment {
+public class FragmentOnline extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view=inflater.inflate(R.layout.fragment_subjects,container,false);
-        final FrameLayout frameLayout = view.findViewById(R.id.fid);
-        CardView
-        return view;
+        return inflater.inflate(R.layout.fragment_online,container,false);
     }
 }
