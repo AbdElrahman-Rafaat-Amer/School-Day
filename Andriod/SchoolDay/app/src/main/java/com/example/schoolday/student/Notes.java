479eaf4853127b935019177e2590f0a6d590819c
Ziad Nasser <55110484+ziad7411@users.noreply.github.com>
2021-04-14 00:44:03 +0200

    13.4

---
+++ Andriod/SchoolDay/app/src/main/java/com/example/schoolday/student/Notes.java
@@ -0,0 +1,41 @@
+package com.example.schoolday.student;
+
+import android.widget.ImageView;
+
+public class Notes {
+
+    String title, description, dateAndTime;
+    ImageView edit;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public ImageView getEdit() {
+        return edit;
+    }
+
+    public void setEdit(ImageView edit) {
+        this.edit = edit;
+    }
+}
