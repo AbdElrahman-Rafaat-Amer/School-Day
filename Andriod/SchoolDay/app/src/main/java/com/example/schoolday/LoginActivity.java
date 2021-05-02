3c390b3110734ba00d4b6fc8f67e02de03669327
Ziad Nasser <55110484+ziad7411@users.noreply.github.com>
2021-05-03 00:00:54 +0200

    complete signup and login

---
+++ Andriod/SchoolDay/app/src/main/java/com/example/schoolday/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.schoolday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ Andriod/SchoolDay/app/src/main/java/com/example/schoolday/Signup2Activity.java
@@ -0,0 +1,14 @@
+package com.example.schoolday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup2);
+    }
+}
