8e7d69c89a683623655da7d1d218001013f4f063
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2021-06-08 07:43:42 +0200

    zoom onilne page

--- Web/BACKEND/LMS/Models/ZoomModel/IZoomRepasitory.cs
+++ Web/BACKEND/LMS/Models/ZoomModel/IZoomRepasitory.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace LMS.Models.ZoomModel
 {
-   public interface IZoomRepasitory
+   public interface IZoomRepasitory<T>
     {
-    
+        void Add(T table);
+        List<T> Links();
+        List<T> ListByFilter(Func<T, bool> lamad);
     }
 }
